f158955ee25c2d9a29a68e283bc7f24b79742988
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-11-30 23:03:04 +0800

    feat: add post module

--- src/drizzle/drizzle.module.ts
+++ src/drizzle/drizzle.module.ts
@@ -4,7 +4,7 @@ import { drizzle, NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schema from './schema/schema';
 
-const DRIZZLE = Symbol('drizzle-connection');
+export const DRIZZLE = Symbol('drizzle-connection');
 @Module({
   providers: [
     {
